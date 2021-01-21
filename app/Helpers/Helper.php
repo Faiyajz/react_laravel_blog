b0535d62aa75663a06526aaacf6f89634f770086
Faiyaj Zaman <faiyajz007@gmail.com>
2021-01-22 01:14:38 +0600

    category controller added and a helper method created

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers;
+
+trait Helper
+{
+    public function slugify($string)
+    {
+        return str_replace(array(" ", '_', '-', ',','#', '$', '&', '@', '*', '^', '"', "'"),'-',$string);
+    }
+}
+
+?>
