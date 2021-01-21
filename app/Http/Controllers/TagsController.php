57c9d8dba48f21cec727c0f2f50e402fc820ebcf
Faiyaj Zaman <faiyajz007@gmail.com>
2021-01-22 01:53:21 +0600

    Tags crud api created

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -2,18 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Tag;
 use Illuminate\Http\Request;
 
 class TagsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->only(['store','update','destroy']);
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if($request->input('all')){
+            $tags = Tag::all();
+        }else{
+            $tags = Tag::paginate(10);
+        }
+        return response()->json(['data'=>$tags],200);
     }
 
     /**
@@ -34,7 +44,20 @@ class TagsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if (!auth("api")->user()->is_admin)
+        {
+            return response()->json(['message'=>'Unathorized'],500);
+        }
+
+        $this->validate($request,[
+            'title'=>'required'
+        ]);
+
+        $tag = new Tag();
+        $tag->title = $request->input('title');
+        $tag->save();
+
+        return  response()->json(['data'=>$tag,'message'=>'Created Successfully'],201);
     }
 
     /**
@@ -45,7 +68,8 @@ class TagsController extends Controller
      */
     public function show($id)
     {
-        //
+        $tag = Tag::findOrFail($id);
+        return response()->json(['data'=>$tag],200);
     }
 
     /**
@@ -68,7 +92,20 @@ class TagsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        if (!auth("api")->user()->is_admin)
+        {
+            return response()->json(['message'=>'Unauthorized'],500);
+        }
+
+        $tag = Tag::findOrFail($id);
+        $this->validate($request,[
+            'title'=>'required'
+        ]);
+
+        $tag->title = $request->input('title');
+        $tag->save();
+
+        return response()->json(['data'=>$tag,'message'=>'Updated Successfully'],200);
     }
 
     /**
@@ -79,6 +116,16 @@ class TagsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        if (!auth("api")->user()->is_admin)
+        {
+            return response()->json(['message'=>'Unauthorized'],500);
+        }
+
+        $tag = Tag::findOrFail($id);
+
+        $tag->delete();
+
+        return response()->json(['message'=>'Deleted Successfully'],200);
+
     }
 }
